60a4a5122e178b37504d6f8e72222463b44524b8
osdf <osendorf@gmail.com>
2013-03-18 12:28:23 +0100

    Modifications to c++ source for cython usage.

--- py_bh_tsne/tsne.cpp
+++ py_bh_tsne/tsne.cpp
@@ -811,38 +811,38 @@ void TSNE::save_data(double* data, int* landmarks, double* costs, int n, int d) 
 
 
 // Function that runs the Barnes-Hut implementation of t-SNE
-int main() {
-    
-    // Define some variables
-	int origN, N, D, no_dims = 2, *landmarks;
-	double perc_landmarks;
-	double perplexity, theta, *data;
-    TSNE* tsne = new TSNE();
-    
-    // Read the parameters and the dataset
-	if(tsne->load_data(&data, &origN, &D, &theta, &perplexity)) {
-        
-		// Make dummy landmarks
-        N = origN;
-        int* landmarks = (int*) malloc(N * sizeof(int));
-        if(landmarks == NULL) { printf("Memory allocation failed!\n"); exit(1); }
-        for(int n = 0; n < N; n++) landmarks[n] = n;
-
-		// Now fire up the SNE implementation
-		double* Y = (double*) malloc(N * no_dims * sizeof(double));
-		double* costs = (double*) calloc(N, sizeof(double));
-        if(Y == NULL || costs == NULL) { printf("Memory allocation failed!\n"); exit(1); }
-		tsne->run(data, N, D, Y, no_dims, perplexity, theta);
-		
-		// Save the results
-		tsne->save_data(Y, landmarks, costs, N, no_dims);
-        
-        // Clean up the memory
-		free(data); data = NULL;
-		free(Y); Y = NULL;
-		free(costs); costs = NULL;
-		free(landmarks); landmarks = NULL;
-    }
-    delete(tsne);
-}
+//int main() {
+//    
+//    // Define some variables
+//	int origN, N, D, no_dims = 2, *landmarks;
+//	double perc_landmarks;
+//	double perplexity, theta, *data;
+//    TSNE* tsne = new TSNE();
+//    
+//    // Read the parameters and the dataset
+//	if(tsne->load_data(&data, &origN, &D, &theta, &perplexity)) {
+//        
+//		// Make dummy landmarks
+//        N = origN;
+//        int* landmarks = (int*) malloc(N * sizeof(int));
+//        if(landmarks == NULL) { printf("Memory allocation failed!\n"); exit(1); }
+//        for(int n = 0; n < N; n++) landmarks[n] = n;
+//
+//		// Now fire up the SNE implementation
+//		double* Y = (double*) malloc(N * no_dims * sizeof(double));
+//		double* costs = (double*) calloc(N, sizeof(double));
+//        if(Y == NULL || costs == NULL) { printf("Memory allocation failed!\n"); exit(1); }
+//		tsne->run(data, N, D, Y, no_dims, perplexity, theta);
+//		
+//		// Save the results
+//		tsne->save_data(Y, landmarks, costs, N, no_dims);
+//        
+//        // Clean up the memory
+//		free(data); data = NULL;
+//		free(Y); Y = NULL;
+//		free(costs); costs = NULL;
+//		free(landmarks); landmarks = NULL;
+//    }
+//    delete(tsne);
+//}
 
