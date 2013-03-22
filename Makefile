d5d7ec0b9cfa99370c98f00abc8653feae5c59af
Justin Bayer <bayer.justin@googlemail.com>
2013-03-22 14:59:06 +0100

    some tidying up, made auto installation work

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all:
 	python setup.py build_ext --inplace
+	python setup.py install
 
 clean :
 	rm -rf *.pyc *.so build/ py_bh_tsne.cpp
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import sys
 if sys.platform == 'darwin':
     ext_modules = [Extension(
         name="py_bh_tsne",
-        sources=["quadtree.cpp", "tsne.cpp", "py_bh_tsne.pyx"],
+        sources=["fasttsne/quadtree.cpp", "fasttsne/tsne.cpp", "fasttsne/py_bh_tsne.pyx"],
         include_dirs = [numpy.get_include()],
         extra_compile_args=['-faltivec', '-I/System/Library/Frameworks/vecLib.framework/Headers'],
         extra_link_args=["-Wl,-framework", "-Wl,Accelerate", "-lcblas"],
@@ -17,7 +17,7 @@ if sys.platform == 'darwin':
 else:
     ext_modules = [Extension(
         name="py_bh_tsne",
-        sources=["quadtree.cpp", "tsne.cpp", "py_bh_tsne.pyx"],
+        sources=["fasttsne/quadtree.cpp", "fasttsne/tsne.cpp", "fasttsne/py_bh_tsne.pyx"],
         include_dirs = [numpy.get_include(), "/usr/local/include"],
         library_dirs = ["/usr/local/lib"],
         extra_compile_args=['-msse2', '-O3', '-fPIC', '-w'],
@@ -28,5 +28,6 @@ else:
 setup(
     name = "py_bh_tsne",
     cmdclass = {"build_ext": build_ext},
-    ext_modules = ext_modules
+    ext_modules = ext_modules,
+    packages=['fasttsne'],
     )
