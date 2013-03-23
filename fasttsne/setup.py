5d75075ba698c70f5a955715486000ec845618dd
osdf <osendorf@gmail.com>
2013-03-23 17:07:38 +0100

    FIX: include_dir didn't point to tsne.h

--- fasttsne/setup.py
+++ fasttsne/setup.py
@@ -18,7 +18,7 @@ else:
     ext_modules = [Extension(
         name="fasttsne",
         sources=["orig-lvdm/quadtree.cpp", "orig-lvdm/tsne.cpp", "fasttsne.pyx"],
-        include_dirs = [numpy.get_include(), "/usr/local/include"],
+        include_dirs = [numpy.get_include(), "/usr/local/include", "orig-lvdm/"],
         library_dirs = ["/usr/local/lib"],
         extra_compile_args=['-msse2', '-O3', '-fPIC', '-w'],
         extra_link_args=["-lopenblas"],
